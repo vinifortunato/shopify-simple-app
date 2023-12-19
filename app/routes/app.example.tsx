03a441b137a3795742ea04565d2ebb3a26978c73
Vinicius Fortunato <vinedfs@outlook.com>
2023-12-19 11:10:30 -0300

    fix: fix example page

--- app/routes/app.example.tsx
+++ app/routes/app.example.tsx
@@ -2,7 +2,6 @@ import { json } from "@remix-run/node";
 import type { LoaderFunctionArgs  } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import {
-  Box,
   Card,
   Layout,
   Page,
@@ -48,22 +47,5 @@ export default function ExamplePage() {
         </Layout.Section>
       </Layout>
     </Page>
-  );
-}
-
-function Code({ children }: { children: React.ReactNode }) {
-  return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
-    >
-      <code>{children}</code>
-    </Box>
   );
 }
